bc081e287b89c696af30a2028bbdd8d4edc1efc9
Lollors <endrewbole18@gmail.com>
2021-05-28 23:37:07 -0300

    Finder elements added

--- src/test/java/pages/ProductListPage.java
+++ src/test/java/pages/ProductListPage.java
@@ -1,4 +1,29 @@
 package pages;
 
-public class ProductListPage {
+import general.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductListPage extends Base {
+    @FindBy(css = "h1.h2Categoria.nomeCategoria")
+    private WebElement lblResult;
+
+    public ProductListPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String readResultBy(){
+        return lblResult.getText();
+    }
+
+    public String readTabTitle(){
+        return driver.getTitle();
+    }
+
+    public void clickAtProduct(String product){
+        driver.findElement(By.xpath("//h3[contains(text(), '"+product+"')]")).click();
+    }
+
 }
